78b43a655136a7f8c14623627d0b95690cfb9ddc
mswiatek <mswiatek@core-logic.com>
2025-01-09 15:58:06 +0100

    generator iterator with BigInteger

--- src/main/java/pl/agh/hasher/PermutationIterator.java
+++ src/main/java/pl/agh/hasher/PermutationIterator.java
@@ -1,16 +1,17 @@
 package pl.agh.hasher;
 
+import java.math.BigInteger;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
 public class PermutationIterator implements Iterator<String> {
 
-    private int currentIndex;
-    private final int endIndex;
+    private BigInteger currentIndex;
+    private final BigInteger endIndex;
     private final int maxLength;
     private final char[] alphabet;
 
-    public PermutationIterator(int startIndex, int endIndex, int maxLength, char[] alphabet) {
+    public PermutationIterator(BigInteger startIndex, BigInteger endIndex, int maxLength, char[] alphabet) {
         this.currentIndex = startIndex;
         this.endIndex = endIndex;
         this.maxLength = maxLength;
@@ -20,7 +21,7 @@ public class PermutationIterator implements Iterator<String> {
     // Sprawdza, czy są jeszcze permutacje do przetworzenia
     @Override
     public boolean hasNext() {
-        return currentIndex <= endIndex;
+        return currentIndex.compareTo(endIndex) <= 0;
     }
 
     // Zwraca następną permutację
@@ -31,19 +32,19 @@ public class PermutationIterator implements Iterator<String> {
         }
 
         String password = getPermutationAtIndex(currentIndex, maxLength, alphabet);
-        currentIndex++;
+        currentIndex = currentIndex.add(BigInteger.ONE); // Przechodzimy do następnego indeksu
         return password;
     }
 
     // Funkcja do uzyskania permutacji na podstawie numeru
-    private String getPermutationAtIndex(int index, int length, char[] alphabet) {
+    private String getPermutationAtIndex(BigInteger index, int length, char[] alphabet) {
         int base = alphabet.length;
         char[] result = new char[length];
 
         for (int i = 0; i < length; i++) {
-            int charIndex = index % base;
-            result[length - 1 - i] = alphabet[charIndex];
-            index /= base;
+            BigInteger charIndex = index.mod(BigInteger.valueOf(base)); // Zmieniamy na BigInteger
+            result[length - 1 - i] = alphabet[charIndex.intValue()]; // Przekształcamy na int
+            index = index.divide(BigInteger.valueOf(base)); // Dzielimy przez podstawę
         }
 
         return new String(result);
--- src/main/java/pl/agh/hasher/PermutationIteratorExample.java
+++ src/main/java/pl/agh/hasher/PermutationIteratorExample.java
@@ -1,16 +1,34 @@
 package pl.agh.hasher;
 
+import java.math.BigInteger;
+
 public class PermutationIteratorExample {
 
     private static char[] alphabet = "abcdefghijklmnopqrstuvwxyz".toCharArray(); // Alfabet
     private static int maxLength = 3; // Maksymalna długość hasła
 
+    public static BigInteger calculateTotalPermutations() {
+        BigInteger total = BigInteger.ZERO;
+        BigInteger currentPower = BigInteger.ONE;
+        BigInteger alphabetSize = BigInteger.valueOf(alphabet.length);
+
+        for (int i = 1; i <= maxLength; i++) {
+            currentPower = currentPower.multiply(alphabetSize);
+            total = total.add(currentPower);
+        }
+
+        return total;
+    }
+
     public static void main(String[] args) {
-        int startIndex = 400;
-        int endIndex = 500;
+        // Używamy BigInteger do reprezentowania indeksów
+        BigInteger startIndex = new BigInteger("400");
+        BigInteger endIndex = new BigInteger("500");
 
         // Tworzymy iterator na podstawie zakresu
         PermutationIterator iterator = new PermutationIterator(startIndex, endIndex, maxLength, alphabet);
+
+        System.out.println("Max permutations: " + calculateTotalPermutations());
 
         // Iterujemy po permutacjach w zadanym zakresie
         while (iterator.hasNext()) {
